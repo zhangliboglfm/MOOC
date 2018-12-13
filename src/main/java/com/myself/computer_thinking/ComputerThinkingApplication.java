95f7ea1ab23ebb0a0a952233cf5113928522a1aa
zlb930711 <zhanglibaoglfm@163.com>
2018-12-13 16:58:33 +0800

    first commit

---
+++ src/main/java/com/myself/computer_thinking/ComputerThinkingApplication.java
@@ -0,0 +1,14 @@
+package com.myself.computer_thinking;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ComputerThinkingApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ComputerThinkingApplication.class, args);
+    }
+
+}
+
