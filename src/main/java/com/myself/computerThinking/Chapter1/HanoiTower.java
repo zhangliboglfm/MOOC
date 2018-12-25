174f8b24b1f6cf35386defc68fbfab874a828d57
zlb930711 <zhanglibaoglfm@163.com>
2018-12-25 16:39:51 +0800

    汉诺塔问题

---
+++ src/main/java/com/myself/computerThinking/Chapter1/HanoiTower.java
@@ -0,0 +1,43 @@
+package com.myself.computerThinking.Chapter1;
+
+import java.util.Iterator;
+import java.util.Stack;
+
+/**
+ * 汉诺塔问题
+ */
+public class HanoiTower {
+    public static void print(Stack<Integer> s) {
+        Iterator<Integer> i = s.iterator();
+        while (i.hasNext()) {
+            System.out.printf("%d ", i.next());
+        }
+        System.out.println();
+    }
+
+    public static void resolve(int n, Stack<Integer> a, Stack<Integer> b, Stack<Integer> c) {
+        if (n==0) return;
+        resolve(n-1, a, c, b);
+        c.push(a.pop());
+        resolve(n-1, b, a, c);
+    }
+
+    public static void main(String[] args) {
+        int count = 32;
+        Stack<Integer> a = new Stack<Integer>();
+        Stack<Integer> b = new Stack<Integer>();
+        Stack<Integer> c = new Stack<Integer>();
+
+        for (int i=count; i>0; i--) {
+            a.push(i);
+        }
+
+        print(a);
+        long start = System.currentTimeMillis();
+        resolve(count, a, b, c);
+        long end = System.currentTimeMillis();
+        print(c);
+
+        System.out.println((end - start)/1000);
+    }
+}
