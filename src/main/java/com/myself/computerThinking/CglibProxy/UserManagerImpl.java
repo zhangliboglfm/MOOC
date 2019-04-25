43d5373186e94406eacdd1c51e8f83a791833a40
zlb930711 <zhanglibaoglfm@163.com>
2019-04-25 16:12:58 +0800

    add cglib proxy

--- src/main/java/com/myself/computerThinking/CglibProxy/UserManagerImpl.java
+++ src/main/java/com/myself/computerThinking/CglibProxy/UserManagerImpl.java
@@ -7,4 +7,7 @@ public class UserManagerImpl {
     public void findUser() {
         System.out.println("*****UserManagerImpl实现类执行*******");
     }
+    public final void addUser() {
+        System.out.println("*****add User*******");
+    }
 }
--- src/main/java/com/myself/computerThinking/CglibProxy/UserMangerProxy.java
+++ src/main/java/com/myself/computerThinking/CglibProxy/UserMangerProxy.java
@@ -9,6 +9,14 @@ import java.lang.reflect.Method;
 public class UserMangerProxy implements MethodInterceptor {
 
 
+    /**
+     * @param obj 代理对象
+     * @param method 委托类方法
+     * @param args  方法参数
+     * @param proxy 代理方法的MethodProxy对象
+     * @return
+     * @throws Throwable
+     */
     @Override
     public Object intercept(Object obj, Method method, Object[] args, MethodProxy proxy) throws Throwable {
         System.out.println("Before:" + method);
@@ -23,6 +31,7 @@ public class UserMangerProxy implements MethodInterceptor {
         enhancer.setCallback(new UserMangerProxy());
         UserManagerImpl userMangerProxy = (UserManagerImpl)enhancer.create();
         userMangerProxy.findUser();
+        userMangerProxy.addUser();
     }
 
 }
