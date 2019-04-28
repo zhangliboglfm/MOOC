5de417bd3a8eb57e3df5220e41475e39e01230f4
zlb930711 <zhanglibaoglfm@163.com>
2019-04-28 10:34:40 +0800

    WebScoket 单个窗口监听和推送

--- src/main/java/com/myself/computerThinking/WebScoket/WebScoketConfig.java
+++ src/main/java/com/myself/computerThinking/WebScoket/WebScoketConfig.java
@@ -1,18 +1,18 @@
-package com.myself.computerThinking.WebScoket;
-
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.web.socket.server.standard.ServerEndpointExporter;
-
-
-/**
- * 开启WebScoket支持
- */
-@Configuration
-public class WebScoketConfig {
-
-    @Bean
-    public ServerEndpointExporter serverEndpointExporter(){
-        return new ServerEndpointExporter();
-    }
-}
+//package com.myself.computerThinking.WebScoket;
+//
+//import org.springframework.context.annotation.Bean;
+//import org.springframework.context.annotation.Configuration;
+//import org.springframework.web.socket.server.standard.ServerEndpointExporter;
+//
+//
+///**
+// * 开启WebScoket支持
+// */
+//@Configuration
+//public class WebScoketConfig {
+//
+//    @Bean
+//    public ServerEndpointExporter serverEndpointExporter(){
+//        return new ServerEndpointExporter();
+//    }
+//}
