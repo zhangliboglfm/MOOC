1d4586a59672a574d38cae464873f79a7713f566
zlb930711 <zhanglibaoglfm@163.com>
2019-05-13 21:00:13 +0800

    update

--- src/main/java/com/myself/JvmMomeory/JvmDemo.java
+++ src/main/java/com/myself/JvmMomeory/JvmDemo.java
@@ -3,8 +3,12 @@ package com.myself.JvmMomeory;
 public class JvmDemo {
 
     public static void main(String[] args) {
+        //为JVM的最大可用内存，可通过-Xmx设置，默认值为物理内存的1/4，设值不能高于计算机物理内存；
         System.out.println(mb(Runtime.getRuntime().maxMemory()));
+        //为当前JVM占用的内存总数，其值相当于当前JVM已使用的内存及freeMemory()的总和，会随着JVM使用内存的增加而增加；
         System.out.println(mb(Runtime.getRuntime().freeMemory()));
+        //为当前JVM空闲内存，因为JVM只有在需要内存时才占用物理内存使用，所以freeMemory()的值一般情况下都很小，
+        // 而 JVM实际可用内存并不等于freeMemory()，而应该等于maxMemory()-totalMemory()+freeMemory()。
         System.out.println(mb(Runtime.getRuntime().totalMemory()));
     }
 
