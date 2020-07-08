185890138232b53d7b555e3e9f09d5585ffb1d40
zlb <zhanglibaoglfm@163.com>
2020-07-08 16:40:35 +0800

    update

--- src/main/java/com/myself/IO/TestInputStream.java
+++ src/main/java/com/myself/IO/TestInputStream.java
@@ -75,5 +75,150 @@ public class TestInputStream {
 
     }
 
+    //从键盘输入字符串，转化为大写，直到输入e或exit退出
+    public static void main(String[] args) {
+        BufferedReader br=null;
+        try {
+            InputStream is=System.in;
+            InputStreamReader isr=new InputStreamReader(is);
+            br = new BufferedReader(isr);
+            String str;
+            while(true){
+                System.out.println("请输入字符串");
+                str=br.readLine();
+                if(str.equalsIgnoreCase("e")||str.equalsIgnoreCase("exit")){
+                    break;
+                }
+                String str1=str.toUpperCase();
+                System.out.println(str1);
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }finally{
+            if(br!=null){
+                try {
+                    br.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+    }
 
+
+    //打印流：字节流 PrintStream  字符流PrintWriter
+    @Test
+    public void printStreamWriter(){
+        FileOutputStream fos=null;
+        try {
+            fos=new FileOutputStream(new File("print.txt"));
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
+        //创建打印输出流，设置为自动刷新模式（写入换行符或字节'\n'时都会刷新输出缓冲区）
+        PrintStream ps=new PrintStream(fos,true);
+        if(ps!=null){
+            System.setOut(ps);//改变输出方式，默认是控制台
+        }
+        for(int i=0;i<=255;i++){//制造内容，输出ASCII字符
+            System.out.print((char)i);
+            if(i%50==0){//每50个数据一行
+                System.out.println();//换行
+            }
+        }
+        ps.close();
+    }
+
+    //进行文件的读、写
+    @Test
+    public void test6(){
+        RandomAccessFile raf1=null;
+        RandomAccessFile raf2=null;
+        try {
+            raf1=new RandomAccessFile(new File("hello.txt"), "r");
+            raf2=new RandomAccessFile(new File("hello1.txt"), "rw");
+            byte[] b=new byte[20];
+            int len;
+            while((len=raf1.read(b))!=-1){
+                raf2.write(b, 0, len);
+            }
+        } catch (IOException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }finally{
+            if(raf2!=null){
+                try {
+                    raf2.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+            if(raf1!=null){
+                try {
+                    raf1.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+    }
+
+    //覆盖相应位置后面的字符
+    @Test
+    public void test7(){
+        RandomAccessFile raf=null;
+        try {
+            raf=new RandomAccessFile(new File("hello1.txt"), "rw");
+            raf.seek(3);//找到3的位置
+            raf.write("xy".getBytes());//在覆盖相应字节数
+
+        } catch (IOException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }finally{
+            if(raf!=null){
+                try {
+                    raf.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+
+    }
+    //实现插入的效果，在d字符后面插入"xy"
+    @Test
+    public void test8(){
+        RandomAccessFile raf=null;
+        try {
+            raf=new RandomAccessFile(new File("hello1.txt"), "rw");
+            raf.seek(4);//找到d的位置
+            String str=raf.readLine();
+//          long l=raf.getFilePointer();
+//          System.out.println(l);
+            raf.seek(4);
+            raf.write("xy".getBytes());//在覆盖相应字节数
+            raf.write(str.getBytes());
+            raf.write(System.getProperty("line.separator").getBytes());
+        } catch (IOException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }finally{
+            if(raf!=null){
+                try {
+                    raf.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+    }
+
+    @Test
+    public void test9(){
+        System.out.println(System.getProperty("line.separator"));
+        System.out.println(System.getProperty("path.separator"));
+        System.out.println(System.getProperty("file.separator"));
+        System.out.println(File.separator);
+    }
 }
